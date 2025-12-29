9a6d14fd2e99841e0000d9737cee6d3124976ab1
Beniamin <beniamin@eklos.ro>
2025-12-29 13:20:21 +0200

    section 8 image upload for vila

--- WhiteLagoon.Web/Controllers/VilasController.cs
+++ WhiteLagoon.Web/Controllers/VilasController.cs
@@ -8,11 +8,15 @@ namespace WhiteLagoon.Web.Controllers
 {
     public class VilasController : Controller
     {
+        private const string DefaultImagePlaceHolder = "https://placehold.co/400";
+        private const string DefaultVilaFolderPath = @"images\Vila";
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public VilasController(IUnitOfWork unitOfWork)
+        public VilasController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -32,11 +36,23 @@ namespace WhiteLagoon.Web.Controllers
             if (vila.Name == vila.Description)
                 ModelState.AddModelError(nameof(Vila.Name), "The description cannot exactly match the Name.");
 
+            ModelState.Remove(nameof(Vila.ImageUrl));
+
             if (!ModelState.IsValid)
             {
                 this.ShowError("Error while creating Vila. Please try again.");
                 return View();
             }
+            if (vila.Image is not null)
+            {
+                var vilaImagePath = vila.Image.GetFullPath(_webHostEnvironment.WebRootPath, DefaultVilaFolderPath);
+                vila.Image.CopyTo(vilaImagePath);
+
+                var fileName = vilaImagePath.GetFileNameFromFullPath();
+                vila.ImageUrl = FileHelper.CreateRootPath(DefaultVilaFolderPath, fileName);
+            }
+            else
+                vila.ImageUrl = DefaultImagePlaceHolder;
 
             _unitOfWork.Villa.Add(vila);
             _unitOfWork.SaveChanges();
@@ -61,6 +77,19 @@ namespace WhiteLagoon.Web.Controllers
         {
             if (ModelState.IsValid && vila.Id > 0)
             {
+                RemoveVilaImageFile(vila);
+
+                if (vila.Image is not null)
+                {
+                    var vilaImagePath = vila.Image.GetFullPath(_webHostEnvironment.WebRootPath, DefaultVilaFolderPath);
+                    vila.Image.CopyTo(vilaImagePath);
+
+                    var fileName = vilaImagePath.GetFileNameFromFullPath();
+                    vila.ImageUrl = FileHelper.CreateRootPath(DefaultVilaFolderPath, fileName);
+                }
+                else
+                    vila.ImageUrl = DefaultImagePlaceHolder;
+
                 _unitOfWork.Villa.Update(vila);
                 _unitOfWork.SaveChanges();
 
@@ -72,6 +101,15 @@ namespace WhiteLagoon.Web.Controllers
             return View(vila);
         }
 
+        private void RemoveVilaImageFile(Vila vila)
+        {
+            if (vila.ImageUrl is not null)
+            {
+                var oldImage = Path.Combine(_webHostEnvironment.WebRootPath, vila.ImageUrl.TrimStart('\\'));
+                if (System.IO.File.Exists(oldImage))
+                    System.IO.File.Delete(oldImage);
+            }
+        }
 
         public IActionResult Delete(int id)
         {
@@ -81,6 +119,8 @@ namespace WhiteLagoon.Web.Controllers
                 this.ShowError("Vila not found.");
                 return RedirectToAction(nameof(HomeController.Error), "Home");
             }
+
+            RemoveVilaImageFile(vilaFromDb);
 
             _unitOfWork.Villa.Remove(vilaFromDb);
             _unitOfWork.SaveChanges();
---
+++ WhiteLagoon.Web/UIHelpers/FileHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace WhiteLagoon.Web.UIHelpers
+{
+    public static class FileHelper
+    {
+        public static string CreateRootPath(string rootPath, string path)
+        {
+            return Path.Combine(rootPath, path);
+        }
+
+        public static string GenerateFileName(this IFormFile file)
+        {
+            var fileExtension = Path.GetExtension(file.FileName);
+            return $"{Guid.NewGuid()}{fileExtension}";
+        }
+
+        public static string GetFullPath(this IFormFile file, string rootPath, string folderPath)
+        {
+            var root = CreateRootPath(rootPath, folderPath);
+            var fileName = file.GenerateFileName();
+
+            return Path.Combine(root, fileName);
+        }
+
+        public static string GetFileNameFromFullPath(this string fullPath) => Path.GetFileName(fullPath);
+
+        public static void CopyTo(this IFormFile file, string filePath)
+        {
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+        }
+    }
+}
